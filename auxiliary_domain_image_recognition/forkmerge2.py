34c346750fd0818be88d23b9af7ffcad2ae5fb00
Diogo Florêncio <>
2024-12-11 08:52:10 -0300

    pp 2024.2 update

--- auxiliary_domain_image_recognition/forkmerge2.py
+++ auxiliary_domain_image_recognition/forkmerge2.py
@@ -12,7 +12,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.backends.cudnn as cudnn
 from torch.utils.data import DataLoader
-from torch.optim.lr_scheduler import CosineAnnealingLR
+from torch.optim.lr_scheduler import StepLR
+from torch.cuda.amp import GradScaler, autocast  # Precision mista
 
 from utils.logger import CompleteLogger
 from utils.data import ForeverDataIterator
@@ -77,34 +78,32 @@ class Trainer:
     def __init__(self, name, model, save_path, optimizer_name, lr, momentum, weight_decay, epochs,
                  task_to_unweighted_probs):
         self.name = name
-        self.model = model
+        self.model = model.to(device)  # Move model to device
         self.save_path = save_path
         self.epochs = epochs
         self.optimizer = utils.get_optimizer(model, optimizer_name, lr, momentum=momentum, weight_decay=weight_decay)
-        self.scheduler = CosineAnnealingLR(self.optimizer, T_max=epochs)
+        self.scheduler = StepLR(self.optimizer, step_size=5, gamma=0.5)  # Mais simples e eficiente
         self.best_acc1 = {}
         self.task_to_unweighted_probs = task_to_unweighted_probs
+        self.scaler = GradScaler()  # Precision mista
 
     def train(self, train_loaders, epoch_start, epoch_end, iters_per_epoch, val_loaders):
         dataset_sampler = SpecifiedProbMultiTaskSampler(train_loaders, 0, self.task_to_unweighted_probs)
         for epoch in range(epoch_start, epoch_end):
-            print(self.scheduler.get_lr())
-            # train for one epoch
-            self.train_one_epoch(dataset_sampler, self.model, self.optimizer, epoch, iters_per_epoch, args, device)
+            print(self.scheduler.get_last_lr())
+            self.train_one_epoch(dataset_sampler, self.model, self.optimizer, epoch, iters_per_epoch, device)
             self.scheduler.step()
 
-            # evaluate on validation set
-            acc1 = utils.validate_all(val_loaders, self.model, args, device)
+            if epoch % 3 == 0:  # Avaliação menos frequente
+                acc1 = utils.validate_all(val_loaders, self.model, args, device)
+                if sum(acc1.values()) > sum(self.best_acc1.values()):
+                    self.save()
+                    self.best_acc1 = acc1
+                print(self.name, "Epoch:", epoch, "lr:", self.scheduler.get_last_lr()[0], "val_criteria:",
+                      round(sum(acc1.values()) / len(acc1), 3),
+                      "best_val_criteria:", round(sum(self.best_acc1.values()) / len(self.best_acc1), 3))
 
-            # remember best acc@1 and save checkpoint
-            if sum(acc1.values()) > sum(self.best_acc1.values()):
-                self.save()
-                self.best_acc1 = acc1
-            print(self.name, "Epoch:", epoch, "lr:", self.scheduler.get_lr()[0], "val_criteria:",
-                  round(sum(acc1.values()) / len(acc1), 3),
-                  "best_val_criteria:", round(sum(self.best_acc1.values()) / len(self.best_acc1), 3))
-
-    def train_one_epoch(self, dataset_sampler, model, optimizer, epoch, iters_per_epoch, args, device):
+    def train_one_epoch(self, dataset_sampler, model, optimizer, epoch, iters_per_epoch, device):
         batch_time = AverageMeter('Time', ':5.2f')
         data_time = AverageMeter('Data', ':5.2f')
         losses = AverageMeter('Loss', ':6.2f')
@@ -114,48 +113,35 @@ class Trainer:
             [batch_time, data_time, losses, accs],
             prefix="Epoch: [{}]".format(epoch))
 
-        # switch to train mode
         model.train()
-
         end = time.time()
         for i in range(iters_per_epoch):
             dataset_name, dataloader = dataset_sampler.pop()
             x, labels = next(dataloader)[:2]
 
-            x = x.to(device)
-            labels = labels.to(device)
-
-            # measure data loading time
+            x, labels = x.to(device), labels.to(device)
             data_time.update(time.time() - end)
 
-            # compute output
-            y = model(x, dataset_name)
-
-            loss = F.cross_entropy(y, labels)
+            with autocast():  # Precision mista
+                y = model(x, dataset_name)
+                loss = F.cross_entropy(y, labels)
 
             acc = accuracy(y, labels)[0]
 
             losses.update(loss.item(), x.size(0))
             accs.update(acc.item(), x.size(0))
 
-            # compute gradient and do SGD step
             optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+            self.scaler.scale(loss).backward()  # Gradientes escalados
+            self.scaler.step(optimizer)
+            self.scaler.update()
 
-            # measure elapsed time
             batch_time.update(time.time() - end)
             end = time.time()
 
-            if i % args.print_freq == 0:
-                progress.display(i)
-
     def test(self, val_loaders):
-        acc1 = utils.validate_all(val_loaders, self.model, args, device)
+        acc1 = utils.validate_all(val_loaders, self.model, device)
         return sum(acc1.values()) / len(acc1), acc1
-
-    def load_best(self):
-        self.model.load_state_dict(torch.load(self.save_path, map_location=device))
 
     def save(self):
         torch.save(self.model.state_dict(), self.save_path)
@@ -169,22 +155,13 @@ def main(args):
         random.seed(args.seed)
         torch.manual_seed(args.seed)
         cudnn.deterministic = True
-        warnings.warn('You have chosen to seed training. '
-                      'This will turn on the CUDNN deterministic setting, '
-                      'which can slow down your training considerably! '
-                      'You may see unexpected behavior when restarting '
-                      'from checkpoints.')
+        cudnn.benchmark = True  # Melhor desempenho com kernel otimizado
 
-    cudnn.benchmark = True
-
-    # Data loading code
     train_transform = utils.get_train_transform(args.train_resizing, random_horizontal_flip=not args.no_hflip,
                                                 random_color_jitter=False, resize_size=args.resize_size,
                                                 norm_mean=args.norm_mean, norm_std=args.norm_std)
     val_transform = utils.get_val_transform(args.val_resizing, resize_size=args.resize_size,
                                             norm_mean=args.norm_mean, norm_std=args.norm_std)
-    print("train_transform: ", train_transform)
-    print("val_transform: ", val_transform)
 
     train_source_datasets, train_target_datasets, val_datasets, test_datasets, num_classes, args.class_names = \
         utils.get_dataset(args.data, args.root, args.source, args.target, train_transform, val_transform)
@@ -197,126 +174,47 @@ def main(args):
                                                           shuffle=True, num_workers=args.workers, drop_last=True)) for
                      name, dataset in train_source_datasets.items()}
 
-    # create model
-    print("=> using model '{}'".format(args.arch))
     backbone = utils.get_model(args.arch, pretrain=not args.scratch)
     pool_layer = nn.Identity() if args.no_pool else None
     heads = nn.ModuleDict({
         dataset_name: nn.Linear(backbone.out_features, num_classes) for dataset_name in args.source
     })
     model = MultiOutputClassifier(backbone, heads, pool_layer=pool_layer, finetune=not args.scratch).to(device)
+
     if args.pretrained is not None:
-        print("Loading from ", args.pretrained)
         checkpoint = torch.load(args.pretrained, map_location=device)
         model.load_state_dict(checkpoint, strict=False)
-    print(heads)
-
-    # resume from the best checkpoint
-    if args.phase != 'train':
-        checkpoint = torch.load(logger.get_checkpoint_path('best'), map_location=device)
-        model.load_state_dict(checkpoint)
-
-    if args.phase == 'test':
-        acc1 = utils.validate_all(test_loaders, model, args, device)
-        print(acc1)
-        return
-
-    init_model_parameters = deepcopy(model.state_dict())
 
     performance_dict = {}
     task_weights = {name: 0 for name in args.source}
     for name in args.target:
         task_weights[name] = 1 / len(args.target)
 
-    for aux_task_name in args.source:
-        if aux_task_name not in args.target:
-            model.load_state_dict(init_model_parameters)
-            new_task_weights = deepcopy(task_weights)
-            new_task_weights[aux_task_name] = 1
-            print(new_task_weights)
-            trainer = Trainer("ranking_{}".format(aux_task_name), model, logger.get_checkpoint_path(aux_task_name),
-                              args.optimizer,
-                              args.lr, args.momentum, args.wd, args.epochs, new_task_weights)
-            trainer.train(train_loaders, 0, args.pruning_epochs, args.iters_per_epoch, val_loaders)
-            performance_dict[aux_task_name] = sum(trainer.best_acc1.values()) / len(trainer.best_acc1)
-
-    ranked_performances = sorted(performance_dict.items(), key=lambda kv: kv[1], reverse=True)
-    ranked_task_names = [task_name for task_name, _ in ranked_performances]
-    print(ranked_performances)
-    print(ranked_task_names)
-
-    model.load_state_dict(init_model_parameters)
-    trainers = {}
-    for topk in args.topk:
-        task_names = ranked_task_names[:topk]
-        new_task_weights = deepcopy(task_weights)
-        for task_name in task_names:
-            new_task_weights[task_name] = 1. / topk
-        print("top {}: {}".format(topk, new_task_weights))
-
-        trainers[topk] = Trainer("top_{}".format(topk), deepcopy(model),
-                                 logger.get_checkpoint_path("top_{}".format(topk)), args.optimizer,
-                                 args.lr,
-                                 args.momentum, args.wd, args.epochs, new_task_weights)
-    target_trainer = list(trainers.values())[0]
-
-    epoch_start = 0
-    epoch_end = args.epoch_step
-    performance_dict = {}
+    epoch_start, epoch_end = 0, args.epoch_step
     theta_dict = {}
+    trainers = {topk: Trainer(f"top_{topk}", deepcopy(model), logger.get_checkpoint_path(f"top_{topk}"),
+                              args.optimizer, args.lr, args.momentum, args.wd, args.epochs, task_weights)
+                for topk in args.topk}
 
     while epoch_start < args.epochs:
-        print("Epoch: {}=>{}".format(epoch_start, epoch_end))
         for name, trainer in trainers.items():
-            print("forking top", name)
             trainer.train(train_loaders, epoch_start, epoch_end, args.iters_per_epoch, val_loaders)
             trainer.load_best()
             theta_dict[name] = deepcopy(trainer.model.state_dict())
             performance_dict[name] = sum(trainer.best_acc1.values()) / len(trainer.best_acc1)
 
-        print("merging")
-        ranked_performances = sorted(performance_dict.items(), key=lambda kv: kv[1], reverse=True)
-        print(ranked_performances)
-        lambda_dict = {name: 0. for name, _ in ranked_performances}
-        lambda_dict[ranked_performances[0][0]] = 1
-
-        criteria_dict = {}
-
-        best_criteria = 0
-
-        for i, (name, _) in enumerate(ranked_performances[1:]):
-            best_lambda = 0
-            upper_bound = sum(lambda_dict.values()) / (i + 1)
-
-            for alpha in args.alphas:
-                new_lambda = alpha * upper_bound
-                lambda_dict[name] = new_lambda
-                theta = sum_all_weights(theta_dict, lambda_dict)
-                target_trainer.model.load_state_dict(theta)
-
-                # evaluate on validation set
-                val_criteria, _ = target_trainer.test(val_loaders)
-
-                # remember best acc@1 and save checkpoint
-                if val_criteria > best_criteria:
-                    best_criteria = val_criteria
-                    best_lambda = new_lambda
-                criteria_dict["{}_{}".format(name, new_lambda)] = val_criteria
-                print("lambda: {}, val_criteria: {}".format(new_lambda, val_criteria))
-            lambda_dict[name] = best_lambda
-        print("Epoch: {} Result: {} Best lambda: {} Best criteria: {}".format(epoch_end, criteria_dict, lambda_dict,
-                                                                              best_criteria))
+        lambda_dict = {name: 0. for name in sorted(performance_dict, key=performance_dict.get, reverse=True)}
+        lambda_dict[list(lambda_dict.keys())[0]] = 1
 
         theta = sum_all_weights(theta_dict, lambda_dict)
-
         for name, trainer in trainers.items():
             trainer.model.load_state_dict(theta)
 
         epoch_start = epoch_end
         epoch_end = min(epoch_end + args.epoch_step, args.epochs)
 
-    test_criteria, test_acc1_dict = target_trainer.test(test_loaders)
-    print("test_performance: {} {}".format(test_criteria, test_acc1_dict))
+    test_criteria, test_acc1_dict = trainers[list(trainers)[0]].test(test_loaders)
+    print("test_performance:", test_criteria, test_acc1_dict)
 
     logger.close()
 
